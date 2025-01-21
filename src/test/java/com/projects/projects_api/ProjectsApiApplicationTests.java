f392c5a820964e055e60e5055c067d01a0636e55
William Reima <william.reima@gmail.com>
2025-01-21 17:05:56 +0200

    inital commit

---
+++ src/test/java/com/projects/projects_api/ProjectsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.projects_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
